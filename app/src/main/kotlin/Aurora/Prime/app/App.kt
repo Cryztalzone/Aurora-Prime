fbec7acd9f1a493b02ab734b5d1e7576c8674a68
Cryztalzone <40550172+Cryztalzone@users.noreply.github.com>
2022-05-23 11:51:54 +0200

    Refactored project to new name

--- app/src/main/kotlin/Aurora/Prime/app/App.kt
+++ app/src/main/kotlin/Aurora/Prime/app/App.kt
@@ -4,7 +4,9 @@ import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 
 fun main() {
-    val builder = JDABuilder.createDefault("NzkxMDU3MTkwODM5OTEwNDMw.X-Jnaw.TnZ3sMwxTIimFZMxy0qZz-Ul1-k")
+    val token = System.getenv("bot_token")
+
+    val builder = JDABuilder.createDefault(token)
     builder.setActivity(Activity.watching("you"))
     builder.build()
 }
